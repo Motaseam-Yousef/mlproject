b37312875e483ea2d2a47405f7a1c7de44f3fa30
Motaseam Yousef <motaseamsaher@gmailcom>
2023-08-10 12:17:31 +0300

    add the custom exception for the error to give more info for errors, tomeasy handling the errors

---
+++ src/exception.py
@@ -0,0 +1,17 @@
+import sys 
+
+def error_message_detail(error):
+    _, _, exc_tb = sys.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occurred in python Script name [{0}] line number [{1}] error message [{2}] ".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message) -> None:
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message)
+    
+    def __str__(self):
+        return self.error_message
