423b1b55587b586197541ca056a360bda384771d
Motaseam Yousef <motaseamsaher@gmailcom>
2023-08-10 12:45:07 +0300

    fix logger file

--- src/logger.py
+++ src/logger.py
@@ -1,14 +1,20 @@
 import logging
 import os
-from datetime import datetime 
+from datetime import datetime
 
+# Define the log file name
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
 
-os.makedirs(logs_path,exist_ok=True)
+# Define the directory path for logs
+logs_dir = os.path.join(os.getcwd(), "logs")
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+# Create the logs directory if it doesn't exist
+os.makedirs(logs_dir, exist_ok=True)
 
+# Define the full path for the log file
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
